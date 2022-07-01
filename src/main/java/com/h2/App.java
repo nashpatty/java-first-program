5da3d1a15391cb45ea0aefcf4cb96f365dbb668c
Niazi <s0ummn@fanniemae.com>
2022-07-01 19:12:10 -0400

    double number method, and add method

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,17 +1,24 @@
 package com.h2;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int[] numbers = {10, 8, 9, 7};
+        int sum = add(numbers);
+        System.out.println("sum = " + sum);
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
+    }
+
+    private static int add(int[] numbers) {
+        return Arrays.stream(numbers)
+                     .reduce(0, Integer::sum);
     }
 }
